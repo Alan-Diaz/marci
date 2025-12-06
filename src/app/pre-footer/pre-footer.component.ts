e9a5072553c72cad5d98e360fb8276efc2e4f3ef
Alan personal <alany.diaz97@gmail.com>
2025-12-06 12:24:42 -0300

    Commit inicial limpio

---
+++ src/app/pre-footer/pre-footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pre-footer',
+  imports: [],
+  templateUrl: './pre-footer.component.html',
+  styleUrl: './pre-footer.component.css'
+})
+export class PreFooterComponent {
+
+}
