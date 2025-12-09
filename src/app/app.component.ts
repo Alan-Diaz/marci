d0d1e7d687fb95a7583177d137e02b066863c89c
Alan personal <alany.diaz97@gmail.com>
2025-12-08 22:23:26 -0300

    added analitycs and firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,  OnInit, inject, PLATFORM_ID } from '@angular/core';
 import { HeadComponent } from './head/head.component';
 import { NavComponent } from './nav/nav.component';
 import { PreFooterComponent } from './pre-footer/pre-footer.component';
@@ -7,6 +7,10 @@ import { ValuesComponent } from './values/values.component';
 import { FooterComponent } from './footer/footer.component';
 import { WorkflowComponent } from './workflow/workflow.component';
 import { environment } from '../environments/environments';
+
+import { isPlatformBrowser } from '@angular/common';
+import { getAnalytics, setUserId } from 'firebase/analytics';
+
 @Component({
   selector: 'app-root',
   imports: [
@@ -24,4 +28,19 @@ import { environment } from '../environments/environments';
 export class AppComponent {
   phone = environment.phone;
   title = 'Marci';
+
+  private platformId = inject(PLATFORM_ID);
+
+  ngOnInit() {
+    if (!isPlatformBrowser(this.platformId)) return; // evita errores SSR
+
+    let userId = localStorage.getItem('user_id');
+    if (!userId) {
+      userId = crypto.randomUUID();
+      localStorage.setItem('user_id', userId);
+    }
+
+    const analytics = getAnalytics();
+    setUserId(analytics, userId);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,21 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAXv1IICJKk612iegwdkoXBj0o6E-NX6Z0",
+  authDomain: "marcimkt-529da.firebaseapp.com",
+  projectId: "marcimkt-529da",
+  storageBucket: "marcimkt-529da.firebasestorage.app",
+  messagingSenderId: "495683893328",
+  appId: "1:495683893328:web:cf7a6e2dc73c02c70eb955",
+  measurementId: "G-VEQNBH49Y8"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
